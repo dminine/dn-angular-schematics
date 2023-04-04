25fe623b0166741024b789a4be0a653d1ef624f5
rlagkrdy <rlagkrdy3883@naver.com>
2023-04-04 23:08:58 +0800

    enhancement: angular v15 배포 수정

---
+++ projects/dn-schematics/src/lib/dn-schematics.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-dn-schematics',
+  template: `
+    <p>
+      dn-schematics works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class DnSchematicsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
