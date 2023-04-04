31fc89804ea9a80cf8155c64ff7001cf18ab6756
rlagkrdy <rlagkrdy3883@naver.com>
2023-04-04 13:27:36 +0800

    enhancement: 테스트중

--- projects/dn-schematics/schematics/pagination-list/index.ts
+++ projects/dn-schematics/schematics/pagination-list/index.ts
@@ -2,7 +2,7 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 
 // 트리를 그대로 반환합니다.
-export function ngAdd(options: any): Rule {
+export function paginationList(options: any): Rule {
   console.log('ng add options', options);
 
   return (tree: Tree, context: SchematicContext) => {
